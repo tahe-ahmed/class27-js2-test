0a73ced84c17fde610069b77534157717d91274a
tahe-ahmed <61438389+tahe-ahmed@users.noreply.github.com>
2020-05-10 14:23:07 +0200

    Update second.js

--- second.js
+++ second.js
@@ -1,11 +1,10 @@
 function logNumbers(startNumber,stopNumber){
-    let i =0;
-    for(i = startNumber;i <= stopNumber; i++){
-        if((i%3 == 0) && (i%5 == 0)){
+    for(let i = startNumber;i <= stopNumber; i++){
+        if((i%3 === 0) && (i%5 === 0)){
             console.log("FizzBuzz");
-        }else if (i%5 == 0){
+        }else if (i%5 === 0){
             console.log("Buzz");
-        }else if (i%3 == 0){
+        }else if (i%3 === 0){
             console.log("Fizz");
         }else{
             console.log(i);
